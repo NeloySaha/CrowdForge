8b948851c5fc2e91a4cf628b9ffff9a9ce0a3c6d
tafsirishraq <tafsirishraq16@gmail.com>
2023-11-24 23:19:11 +0600

    partial HR

--- backend/index.js
+++ backend/index.js
@@ -220,3 +220,90 @@ app.get("/pendingReq/:club", (req, res) => {
     return res.json(data);
   });
 });
+
+//HR accept request
+
+app.post("/hrMemInsert", (req, res) => {
+  const {
+    name,
+    email,
+    dob,
+    department,
+    gender,
+    club,
+    password,
+    contact_no,
+    evaluation,
+  } = req.body;
+
+  const sql1 =
+    "INSERT into member (name,designation,email,dob,department,gender,club,password,contact_no,rating,evaluation) Values (?,'general',?,?,?,?,?,?,?,0,?)";
+
+  const sql2 = "DELETE FROM incoming_request WHERE email = ? and club = ?";
+
+  db.query(
+    sql1,
+    [
+      name,
+      email,
+      dob,
+      department,
+      gender,
+      club,
+      password,
+      contact_no,
+      evaluation,
+    ],
+    (err1, data1) => {
+      if (err1) {
+        return res.send("Couldn't insert");
+      }
+
+      db.query(sql2, [email, club], (err2, data2) => {
+        if (err2) return res.send("Couldn't remove the pending request");
+
+        return res.send("🎉Member now Approved");
+      });
+    }
+  );
+});
+
+//HR reject request
+app.post("/hrRejectReq", (req, res) => {
+  const { email, club } = req.body;
+  const sql = "DELETE FROM incoming_request WHERE email = ? and club = ?";
+
+  db.query(sql, [email, club], (err, data) => {
+    if (err) return res.send("Sorry! Couldn't remove the request");
+
+    return res.send("Request removed.");
+  });
+});
+
+//HR monitoring
+
+app.get("/hrMonitor/:club", (req, res) => {
+  const { club } = req.params;
+  const sql = "SELECT * FROM member WHERE club=? AND designation='general'";
+
+  db.query(sql, [club], (err, data) => {
+    if (err) return res.json(err);
+
+    return res.json(data);
+  });
+});
+
+//Update member rating
+
+app.post("/updateRating", (req, res) => {
+  const { email, club, newRating } = req.body;
+  const sql = "UPDATE member set rating=? WHERE email = ? AND club = ?";
+
+  db.query(sql, [newRating, email, club], (err, data) => {
+    if (err) return res.send("Sorry! Couldn't update rating");
+
+    return res.send("🎉Successfully updated the rating");
+  });
+});
+
+//
